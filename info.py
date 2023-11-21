322aef73df9bf48cfa521329706dfc0f51ee8004
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2023-11-21 09:02:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,7 +80,7 @@ MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-LANGUAGES = ["hindi", "480p", "english", "720p" ,"tamil", "1080p", "telugu", "480p 10bit", "malayalam", "720p 10bit", "kannada", "1080p 10bit", "Punjabi", "720p HEVC", "Bengali", "1080p HEVC", "PreDvD", "Web-DL"]
+LANGUAGES = ["hindi", "480p", "english", "720p" ,"tamil", "1080p", "telugu", "480p 10bit", "malayalam", "720p 10bit", "kannada", "1080p 10bit", "Punjabi", "720p HEVC", "Bengali", "1080p HEVC", "Pre", "Web"]
 
 SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
 
