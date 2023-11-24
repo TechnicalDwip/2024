3bcec1ad3ffeb62058f7e15b837fae29bbdb25bb
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2023-11-24 19:26:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -950,7 +950,7 @@ async def shortlink(bot, message):
         return await message.reply(f"You are anonymous admin. Turn off anonymous admin and try again this command")
     chat_type = message.chat.type
     if chat_type == enums.ChatType.PRIVATE:
-        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\nSee Sample:-\n``/shortlink GreyMatterslinks.in 3b7014f145818b00af237574d918cdabbe523488``\n\nThat's it!!! Enjoy Earning Money 💲\n\n[[[ Trusted Earning Site - https://GreyMatterslinks.in]]]\n\nIf you have any Doubts, Feel Free to Ask me - @RoyalDwip</b>")
+        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\nSee Sample:-\n``/shortlink {anytype of shortlink} {Your Api key}\n\nThat's it!!! Enjoy Earning Money 💲\n\nIf you have any Doubts, Feel Free to Ask me - @RoyalDwip</b>")
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         grpid = message.chat.id
         title = message.chat.title
