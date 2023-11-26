eec910a3bbd65775c1950962b1711bab0591494e
Royal Dwip <120001510+Royal-Luku@users.noreply.github.com>
2023-11-26 11:38:02 +0530

    Update check_alive.py

--- plugins/Extra/check_alive.py
+++ plugins/Extra/check_alive.py
@@ -4,9 +4,9 @@ from pyrogram import Client, filters
 
 CMD = ["/", "."]
 
-@Client.on_message(filters.command("alive", CMD))
+@Client.on_message(filters.command("tutorial", CMD))
 async def check_alive(_, message):
-    await message.reply_text("<b>Not dead but still here.. You have no love for me now. 💔 Good.. You are not the same as you were before..‌‌ 😔\n\nPress /Start</b>")
+    await message.reply_text("<b>Think You Don't Know How To Verify <a href='https://t.me/ShinchanFilterRobot'>Me</a>! \nYou Can Watch This Video 🔥\nVideo Link 🔗 - || youtu.be/tLruG2ZNqJo?si=Y9vPy6ILQzt3xVVu ||</b>")
 
 
 @Client.on_message(filters.command("ping", CMD))
