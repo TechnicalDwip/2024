07aae8f8f029345e46f0a0a746229eb0d46191c1
Royal Dwip <130228344+TechnicalDwip@users.noreply.github.com>
2023-12-26 15:27:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -432,12 +432,14 @@ async def start(client, message):
         f_caption = f"[ @WOMBACKUP ]  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
     if not await check_verification(client, message.from_user.id) and VERIFY == True:
         btn = [
-    [
+            [
         InlineKeyboardButton('⚠️ Verify', url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=")
-        ],[
+        
+                             ],
+               [
         InlineKeyboardButton('How To Verify ❓', url="https://youtu.be/tLruG2ZNqJo?si=Y9vPy6ILQzt3xVVu")
     ]
-]
+        ]
         await message.reply_text(
             text="<b>•<i> Your Temporary Token has been expired, Kindly generate a New Temporary Token By Using Verify Button!</i>\n\nToken Timeout : Today Midnight 🕛</b>",
             protect_content=True,
